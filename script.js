ec0eed47a194fa18cd74a86cc11ba6ade718e994
Sego <s.henniart.webdev@gmail.com>
2023-06-27 11:31:09 +0200

    fixed Sass + go get data with php

--- script.js
+++
@@ -1,7 +0,0 @@
-function highlightSelectedOption() {
-    var selectElement = document.getElementById("favTroop");
-    var selectedOption = selectElement.options[selectElement.selectedIndex];
-    selectedOption.style.backgroundColor = "gold";
-    selectedOption.style.color = "green";
-  }
-//   /!\ pas encore importé dans html
